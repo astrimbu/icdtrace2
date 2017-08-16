946dc2cf2f71c4f44e4c2f126870830300aee898
Alan Strimbu <astrimbu@gmail.com>
2017-08-16 09:50:22 -0500

    DiagnosisForm, rm App.js, form alerts value

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import DiagnosisForm from './DiagnosisForm';
 import './styles/app.css';
-import App from './App';
+import logo from './logo.png';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="Seahorse Logo" />
+          <h2>ICDTrace</h2>
+        </div>
+        <p className="App-intro">
+          Enter Diagnosis:
+        </p>
+        <DiagnosisForm />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+);
+
 registerServiceWorker();
